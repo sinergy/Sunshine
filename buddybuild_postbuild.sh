f03d7e865f1a169033e5b064a12c1d11f20c047b
Cloud Chen <cloudchen@yahoo-inc.com>
2017-09-20 18:52:48 +0800

    Add CoPilot script for buddybuild

---
+++ buddybuild_postbuild.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# If your build needs more advanced behavior, see detailed buddybuild instructions:
+# https://docs.buddybuild.com/builds/index.html
+
+bash <(curl -s https://copilot.blackducksoftware.com/ci/buddybuild/scripts/upload)
